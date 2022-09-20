1a2742627231bc63645822fba83ce4900a812240
myding <johndoe@example.com>
2022-09-20 11:58:49 +0200

    Wrote the program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,58 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string fileName = "students.txt";
+        if (args.Length > 0)
+        {
+            fileName = args[0];
+        }
+
+        IEnumerable<string> studentsFromFile;
+        try
+        {
+            studentsFromFile = File.ReadLines(fileName);
+        }
+        catch
+        {
+            Console.WriteLine("No student list found, using demo list.");
+            Console.ReadKey();
+            studentsFromFile = new List<string>() { "Stefan", "Gun", "Tröstur" };
+        }
+
+        Random randomGenerator = new();
+        List<string> studentsToAsk = new();
+
+        while (true)
+        {
+            studentsToAsk = new List<string>(studentsFromFile);
+
+            while (studentsToAsk.Count > 0)
+            {
+                Console.Clear();
+                int randomNumber = randomGenerator.Next(studentsToAsk.Count);
+                string student = studentsToAsk[randomNumber];
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Kvar att fråga: " + studentsToAsk.Count);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(student);
+                studentsToAsk.Remove(student);
+
+                //Kolla om vi trycker ESC, isf avsluta programmet
+                var key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.Escape)
+                {
+                    Console.ResetColor();
+                    Environment.Exit(0);
+                }
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("Listan är slut. Vill du börja om? (j/N)");
+            if (Console.ReadKey().Key != ConsoleKey.J)
+            {
+                break;
+            }
+        }
     }
 }
